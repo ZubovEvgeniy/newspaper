ece072d8cdc75b17325eb8159105abd985772485
Evgeniy <valzubof@yandex.ru>
2023-06-29 12:14:47 +0400

    upв settings

--- news/news/settings.py
+++ news/news/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-(-ut$2prcjs3q(45w0ehv(n(#*5gi5_l3=py7ngc#2j16et-r2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
